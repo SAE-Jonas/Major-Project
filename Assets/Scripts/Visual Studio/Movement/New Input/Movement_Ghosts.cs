10d92dc1428dad09faa7c66080c3b0bb73f04e5f
Jonas Joshua Noronha <j.noronha@student.saedubai.com>
2021-03-12 15:37:55 +0400

    Reogranised Folders

---
+++ Assets/Scripts/Visual Studio/Movement/New Input/Movement_Ghosts.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement_Ghosts : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
