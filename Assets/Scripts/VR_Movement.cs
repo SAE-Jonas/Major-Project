f7d64a5ee490246ceda749e77efbca3536d28540
Jonas Joshua Noronha <j.noronha@student.saedubai.com>
2021-03-21 14:43:24 +0400

    VR Movement Setup

---
+++ Assets/Scripts/VR_Movement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VR_Movement : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 12f;
+    public float gravity = -98.1f;
+
+    Vector3 velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        float x = Input.GetAxis("Horizontal3");
+        float z = Input.GetAxis("Vertical3");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
