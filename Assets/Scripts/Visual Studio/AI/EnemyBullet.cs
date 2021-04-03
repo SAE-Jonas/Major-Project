0757975d31e2f98e13096f999e7941b05a399a95
Jonas Joshua Noronha <j.noronha@student.saedubai.com>
2021-04-03 21:41:59 +0400

    Ghost Player Health

---
+++ Assets/Scripts/Visual Studio/AI/EnemyBullet.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBullet : MonoBehaviour
+{
+    public int BulletDamage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision player)
+    {
+        if (player.gameObject.tag == "Player")
+        {
+            player.gameObject.GetComponent<PlayerHealth>().DamageRecieved(BulletDamage);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Visual Studio/AI/PlayerHealth.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int PlayerStartHealth;
+    public int PlayerGameHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerGameHealth = PlayerStartHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlayerGameHealth <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    public void DamageRecieved(int HealthReduced)
+    {
+        PlayerGameHealth -= HealthReduced;
+    }
+}
